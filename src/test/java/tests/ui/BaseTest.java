8653210c447fb2de367ad57ff17c54b364940d7a
Michael Bedoya <icmichael.bedoya@americanwell.com>
2024-12-09 14:28:52 -0500

    Feature: base classes and init config created

---
+++ src/test/java/tests/ui/BaseTest.java
@@ -0,0 +1,27 @@
+package tests.ui;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import utils.DriverManager;
+
+public class BaseTest {
+    protected final static String baseURL = "https://www.bookdepository.com/";
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setUp(@Optional("chrome") String browser){
+        DriverManager.setDriver(browser);
+        driver = DriverManager.getDriver();
+        driver.get(baseURL);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        DriverManager.quitDriver();
+    }
+}
