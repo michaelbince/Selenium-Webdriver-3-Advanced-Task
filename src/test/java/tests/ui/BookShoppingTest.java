03050877481d62ddc12715ecbcf24830626a7fb2
Michael Bedoya <icmichael.bedoya@americanwell.com>
2024-12-10 15:51:45 -0500

    Feature: Test verifyBookIsAddedToTheBasket added

--- src/test/java/tests/ui/BookShoppingTest.java
+++ src/test/java/tests/ui/BookShoppingTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import pages.BookPage;
 import pages.SearchComponentPage;
 import pages.SearchedResultsPage;
+import pages.ShoppingCartPage;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class BookShoppingTest extends BaseTest {
     private SearchComponentPage searchComponentPage;
     private SearchedResultsPage searchedResultsPage;
     private BookPage bookPage;
+    private ShoppingCartPage shoppingCartPage;
 
 
     @BeforeMethod
@@ -20,6 +22,7 @@ public class BookShoppingTest extends BaseTest {
         searchComponentPage = new SearchComponentPage();
         searchedResultsPage = new SearchedResultsPage();
         bookPage = new BookPage();
+        shoppingCartPage = new ShoppingCartPage();
     }
 
     @Test
@@ -42,5 +45,17 @@ public class BookShoppingTest extends BaseTest {
         Assert.assertFalse(firstSearchedResults.equals(secondSearchedResults), "The book titles should be different");
     }
 
-
+    @Test
+    public void verifyBookIsAddedToTheBasket(){
+        searchComponentPage.search("Thinking in Java");
+        searchedResultsPage.selectEnglishLanguageFilter();
+        searchedResultsPage.clickBookTitleByIndex(1);
+        Assert.assertTrue(bookPage.getBookAddedTittle().contains("Thinking in Java"),
+                "Book title does not contain the expected book searched: 'Thinking in Java'.");
+        bookPage.addBookToCart();
+        searchComponentPage.clickCartButton();
+        Assert.assertTrue(shoppingCartPage.getBookInCartTittle().contains("Thinking in Java"),
+                "Book title does not contain the expected book searched: 'Thinking in Java'.");
+        shoppingCartPage.proceedToCheckoutWithAddedBooks();
+    }
 }
